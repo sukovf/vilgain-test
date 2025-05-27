f72ea5b5e4192ab2f8f1c23b6ddf00d1c0bbc589
msoucek <info@soucekmartin.cz>
2025-05-27 21:04:02 +0200

    4 - Created endpoints to get all Users or a single User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Api\Response;
+use App\Security\UserRole;
 use App\Service\User\Facade;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Post;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserController extends AbstractFOSRestController
 {
@@ -21,5 +24,19 @@ class UserController extends AbstractFOSRestController
         return new Response([
             'newUserId' => $newUserId
         ], SymfonyResponse::HTTP_CREATED);
+    }
+
+    #[IsGranted(UserRole::ADMIN->value)]
+    #[Get('/users')]
+    public function getUsers(): Response
+    {
+        return new Response($this->userFacade->getAll());
+    }
+
+    #[IsGranted(UserRole::ADMIN->value)]
+    #[Get('/users/{id}')]
+    public function getOneUser(int $id): Response
+    {
+        return new Response($this->userFacade->getOne($id));
     }
 }
--- src/Service/User/Facade.php
+++ src/Service/User/Facade.php
@@ -2,15 +2,32 @@
 
 namespace App\Service\User;
 
+use App\Entity\User;
 use App\Service\User\Creator\Creator;
 use Symfony\Component\HttpFoundation\Request;
 
 class Facade
 {
-    public function __construct(private readonly Creator $creator) {}
+    public function __construct(
+        private readonly Creator $creator,
+        private readonly Getter  $getter
+    ) {}
 
     public function create(Request $request): int
     {
         return $this->creator->create($request);
+    }
+
+    /**
+     * @return User[]
+     */
+    public function getAll(): array
+    {
+        return $this->getter->getAll();
+    }
+
+    public function getOne(int $id): User
+    {
+        return $this->getter->getOne($id);
     }
 }
