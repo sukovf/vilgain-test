f05feeb166a096b5c1cebc66c7e27c4d1fbf9468
msoucek <info@soucekmartin.cz>
2025-05-27 12:42:25 +0200

    1 - Added Doctrine-related dependencies

--- Docker/web_server/entrypoint.sh
+++ Docker/web_server/entrypoint.sh
@@ -9,5 +9,18 @@ chown www-data:www-data ./var/log ./var/cache
 # install PHP dependencies
 composer install
 
+# wait for the DB to warmup
+/usr/local/bin/wait_for_it.sh db_server:3306 -t 30
+
+# run Doctrine migrations if the dev DB isn't up to date
+if [[ $(php ./bin/console doctrine:migrations:up-to-date) != *"[OK]"* ]]; then
+  php ./bin/console doctrine:migrations:migrate --no-interaction
+fi
+
+# run Doctrine migrations if the test DB isn't up to date
+if [[ $(php ./bin/console doctrine:migrations:up-to-date --env=test) != *"[OK]"* ]]; then
+  php ./bin/console doctrine:migrations:migrate --no-interaction --env=test
+fi
+
 # run Apache server
 apachectl -D FOREGROUND
